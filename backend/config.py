d7e0d07ccdef7344cee976aa3f1402a7efd693a0
Rajnil <rjmbu123@gmail.com>
2026-08-30 23:17:17 +0530

    frontend

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,25 @@
 import os
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
+    database_url: str
+    redis_url: str
+
+    groq_api_key: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+settings = Settings()
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
